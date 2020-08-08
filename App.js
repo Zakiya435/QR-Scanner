ebf9cf1b8d746af1f42bd662491ecea28477156b
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2020-08-08 21:35:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+import ScanScreen from './screens/ScanScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (    
+       
+      
+        <AppContainer />
+        
+      
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
